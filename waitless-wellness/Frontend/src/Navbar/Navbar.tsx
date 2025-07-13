57639eb8834eacd2535d79788b56a1d9d668850a
Theo <theo.osinfolarin@gmail.com>
2025-07-13 10:49:20 +0100

    Add naming for navbar

--- waitless-wellness/Frontend/src/Navbar/Navbar.tsx
+++ waitless-wellness/Frontend/src/Navbar/Navbar.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import naming from "/Users/theoosinfolarin/Desktop/Waitless-Wellness/waitless-wellness/Frontend/src/Naming.json";
+
 
 export const Navbar: React.FC = () => {
   return (
     <header className="navbar">
       <nav>
         <ul className="navbar-links">
-          <li><a href="/">Home</a></li>
+          <li><a href="/">{naming.navbar.Homepage}</a></li>
           <li><a href="/symptoms">Symptoms</a></li>
-          <li><a href="/faqs">FAQs</a></li>
+          <li><a href="/faqs">{naming.navbar.FAQPage}</a></li>
+          <li><a href="/closestClinics">{naming.navbar.NearestClinics}</a></li> // API to find phsyios
           <li><a href="/signup">Sign Up</a></li>
           <li><a href="/login">Login</a></li>
-          
         </ul>
       </nav>
     </header>
