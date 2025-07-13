e678360c72174ce47fdb1451c417d570a9469566
Theo <theo.osinfolarin@gmail.com>
2025-07-13 21:14:31 +0100

    complete navbar options

--- waitless-wellness/Frontend/src/Navbar/Navbar.tsx
+++ waitless-wellness/Frontend/src/Navbar/Navbar.tsx
@@ -8,11 +8,11 @@ export const Navbar: React.FC = () => {
       <nav>
         <ul className="navbar-links">
           <li><a href="/">{naming.navbar.Homepage}</a></li>
-          <li><a href="/symptoms">Symptoms</a></li>
+          <li><a href="/symptoms">{naming.navbar.Symptoms}</a></li>
           <li><a href="/faqs">{naming.navbar.FAQPage}</a></li>
-          <li><a href="/closestClinics">{naming.navbar.NearestClinics}</a></li> // API to find phsyios
-          <li><a href="/signup">Sign Up</a></li>
-          <li><a href="/login">Login</a></li>
+          <li><a href="/closestClinics">{naming.navbar.NearestClinics}</a></li> API to find phsyios
+          <li><a href="/signup">{naming.navbar.Signup}</a></li>
+          <li><a href="/login">{naming.navbar.Login}</a></li>
         </ul>
       </nav>
     </header>
