50f1f0d0455364a97d7dd75b84a9861d36305704
Theo <theo.osinfolarin@gmail.com>
2025-08-30 01:22:11 +0100

    more changes and displaying FAQs

--- waitless-wellness/Frontend/src/LoginPage.tsx
+++ waitless-wellness/Frontend/src/LoginPage.tsx
@@ -9,22 +9,12 @@ const LoginPage: React.FC = () => {
         <h1 className="text-4xl md:text-5xl font-bold mb-6">
           <span className="text-blue-600">Waitless Wellness Login</span>
         </h1>
-        <p className="text-lg md:text-xl mb-8">
-         Login
-        </p>
-
         <div className="flex gap-4 justify-center">
-          <Link
-            to="/symptoms"
-            className="px-6 py-3 bg-blue-600 text-white rounded-2xl shadow hover:bg-blue-700 transition"
-          >
-            Check Symptoms
-          </Link>
           <Link
             to="/closestClinics"
             className="px-6 py-3 bg-gray-200 text-gray-800 rounded-2xl shadow hover:bg-gray-300 transition"
           >
-            Login
+            Login Form
           </Link>
         </div>
       </section>
--- waitless-wellness/Frontend/src/SymptomPage.tsx
+++ waitless-wellness/Frontend/src/SymptomPage.tsx
@@ -13,21 +13,6 @@ const SymptomPage: React.FC = () => {
           Your trusted companion for tracking symptoms, finding nearby clinics, 
           and accessing reliable health information.
         </p>
-
-        <div className="flex gap-4 justify-center">
-          <Link
-            to="/symptoms"
-            className="px-6 py-3 bg-blue-600 text-white rounded-2xl shadow hover:bg-blue-700 transition"
-          >
-            Check Symptoms
-          </Link>
-          <Link
-            to="/closestClinics"
-            className="px-6 py-3 bg-gray-200 text-gray-800 rounded-2xl shadow hover:bg-gray-300 transition"
-          >
-            Find Clinics
-          </Link>
-        </div>
       </section>
     </main>
   );
